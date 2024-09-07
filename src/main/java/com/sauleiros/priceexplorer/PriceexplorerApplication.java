5cdf61de4d82437d78de4b1dc2debbd9e0f54a67
sauleiros <sauleiros@gmail.com>
2024-09-07 04:07:02 +0200

    feat: Added base project created from spring initializr

---
+++ src/main/java/com/sauleiros/priceexplorer/PriceexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.sauleiros.priceexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceexplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sauleiros/priceexplorer/PriceexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sauleiros.priceexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
